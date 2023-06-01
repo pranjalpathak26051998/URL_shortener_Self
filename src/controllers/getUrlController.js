5470025cfab30c38b26ac7dcd21b34099da09306
https://github.com/pranjalpathak26051998 <pranjalpathak010@gmail.com>
2023-06-02 02:38:27 +0530

    added node-cache

--- src/controllers/getUrlController.js
+++ src/controllers/getUrlController.js
@@ -1,15 +1,30 @@
 const urlModel=require('../models/urlModel')
-const validURl=require('valid-url');
-const shortId=require('shortid');
+const nodeCache=require('node-cache')
 
+const cache=new nodeCache() // initializing a cache memory
 
 const getUrl=async (req,res)=>{
     try {
         let {urlCode}=  req.params
+
         if(!urlCode) return res.status(400).send({status:false,message:"please provide the urlCode"})
-    let url = await urlModel.findOne({urlCode:urlCode})
-    let resultant=url.longUrl
-    return res.redirect(resultant)  
+    
+//cache enters here
+  //get the cache memory here and get the data into it
+  const cachedUrl = cache.get(urlCode);
+  if(cachedUrl){
+    return res.status(200).redirect(cachedUrl)
+  }
+
+       let url = await urlModel.findOne({urlCode:urlCode})
+     //add url not found validation
+     if(!url){
+        return res.status(404).send({status:false,message:`URL NOT FOUND`})
+     }
+     cache.set(urlCode,url.longUrl,60) //set the timer and response via cache
+     
+        let resultant=url.longUrl
+    return res.status(200).redirect(resultant)  
         
     } catch (error) {
         res.status(500).send({status:false,message:`error detected${error.message}`})
