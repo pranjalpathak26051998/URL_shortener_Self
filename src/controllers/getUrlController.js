946dd04add621fffaf34d42547ac1163e2945ab0
https://github.com/pranjalpathak26051998 <pranjalpathak010@gmail.com>
2023-06-01 10:24:48 +0530

    trycatch

--- src/controllers/getUrlController.js
+++ src/controllers/getUrlController.js
@@ -4,9 +4,16 @@ const shortId=require('shortid');
 
 
 const getUrl=async (req,res)=>{
-    let {urlCode}=  req.params
+    try {
+        let {urlCode}=  req.params
     let url = await urlModel.findOne({urlCode:urlCode})
     let resultant=url.longUrl
     return res.status(302).send({status:true,data:resultant})  
+        
+    } catch (error) {
+        res.status(500).send({status:false,message:`error detected${error.message}`})
+        
+    }
+    
 }
 module.exports.getUrl=getUrl
--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -1,27 +1,34 @@
-const urlModel=require('../models/urlModel')
-const validURl=require('valid-url');
-const shortId=require('shortid');
+const urlModel = require('../models/urlModel')
+const validURl = require('valid-url');
+const shortId = require('shortid');
 
-const urlControl=async (req,res)=>{
-   const {longUrl}=req.body
-   if (!validURl.isWebUri(longUrl)) {
-    return res.status(400).json({ error: 'Invalid URL' });
+const urlControl = async (req, res) => {
+    try {
+        const { longUrl } = req.body
+        if (!validURl.isWebUri(longUrl)) {
+            return res.status(400).json({ error: 'Invalid URL' });
+        }
+        const baseUrl = "localhost:3000/"
+        //shortURl=baseUrl+urlCode concatenate
+        //generate urlCode
+        const urlCode = shortId.generate(longUrl);
+        console.log(urlCode); // Output: Something like "r4rGze22"
+        //"http://localhost:3000/ghfgfg",
+        const shortUrl = `${baseUrl}/${urlCode}`;
+        //const response={longUrl:longUrl,shortUrl:shortUrl,urlCode:urlCode}
+        savedResponse = await urlModel.create({
+            longUrl,
+            shortUrl,
+            urlCode,
+        })
+        return res.status(201).send({ status: true, data: savedResponse })
+
+    } catch (error) {
+        res.status(500).send({ status: false, message: `Error detected : ${error.message}` })
+
+    }
+
 }
-    const baseUrl="localhost:3000/"
-    //shortURl=baseUrl+urlCode concatenate
-    //generate urlCode
-    const urlCode = shortId.generate(longUrl);
-    console.log(urlCode); // Output: Something like "r4rGze22"
-   //"http://localhost:3000/ghfgfg",
-    const shortUrl=`${baseUrl}/${urlCode}`;
-    //const response={longUrl:longUrl,shortUrl:shortUrl,urlCode:urlCode}
-    savedResponse=await urlModel.create({
-        longUrl,
-        shortUrl,
-        urlCode,
-    })
-    return res.status(201).send({status:true,data:savedResponse})
-}
-module.exports.urlControl=urlControl
+module.exports.urlControl = urlControl
 
 
