d10c0c17e58c65a25ba71bcc0232f3f5b8b6e16e
https://github.com/pranjalpathak26051998 <pranjalpathak010@gmail.com>
2023-06-01 12:39:12 +0530

    added new changes

--- src/controllers/getUrlController.js
+++ src/controllers/getUrlController.js
@@ -6,9 +6,10 @@ const shortId=require('shortid');
 const getUrl=async (req,res)=>{
     try {
         let {urlCode}=  req.params
+        if(!urlCode) return res.status(400).send({status:false,message:"please provide the urlCode"})
     let url = await urlModel.findOne({urlCode:urlCode})
     let resultant=url.longUrl
-    return res.status(302).send({status:true,data:resultant})  
+    return res.redirect(resultant)  
         
     } catch (error) {
         res.status(500).send({status:false,message:`error detected${error.message}`})
--- src/controllers/urlController.js
+++ src/controllers/urlController.js
@@ -5,16 +5,26 @@ const shortId = require('shortid');
 const urlControl = async (req, res) => {
     try {
         const { longUrl } = req.body
-        if (!validURl.isWebUri(longUrl)) {
-            return res.status(400).json({ error: 'Invalid URL' });
-        }
-        const baseUrl = "localhost:3000/"
+        if(!longUrl) return res.status(400).send({status:false,message:"Url not found or URL not provided"})
+      ///  if(typeof longUrl !== String) return res.status(400).send({status:false,message:"Url not in a String"})
+         
+      if (!validURl.isWebUri(longUrl)) {
+        return res.status(400).json({ error: 'Invalid URL' });
+    }
+        let checkUrl= await urlModel.findOne({longUrl:longUrl})
+       // console.log(checkUrl)
+        if(checkUrl) return res.status(200).send({status:true,data:checkUrl.shortUrl}) // reconfirm status code
+          
+        
+        const baseUrl = " http://localhost:3000/"
+        if(baseUrl) return res.status(400).send({status:false,message:"Invalid request, please provide baseUrl"})
         //shortURl=baseUrl+urlCode concatenate
         //generate urlCode
         const urlCode = shortId.generate(longUrl);
+    
         console.log(urlCode); // Output: Something like "r4rGze22"
         //"http://localhost:3000/ghfgfg",
-        const shortUrl = `${baseUrl}/${urlCode}`;
+        const shortUrl = `${baseUrl}${urlCode}`;
         //const response={longUrl:longUrl,shortUrl:shortUrl,urlCode:urlCode}
         savedResponse = await urlModel.create({
             longUrl,
